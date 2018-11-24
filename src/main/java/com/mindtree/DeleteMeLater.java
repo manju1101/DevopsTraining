e4b6c04e240ae06b4be39eb7db5c09d37e39e8b3
Catalina\mramu <Catalina-1>
2018-11-24 11:45:39 +0530

    Added new class file

---
+++ src/main/java/com/mindtree/DeleteMeLater.java
@@ -0,0 +1,18 @@
+/**
+ * Author : M1040615
+ * Create date : Nov 24, 2018 | 11:45:03 AM
+ * Modified : 11:45:03 AM
+ */
+package com.mindtree;
+
+public class DeleteMeLater {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Delete Me");
+
+	}
+
+}
